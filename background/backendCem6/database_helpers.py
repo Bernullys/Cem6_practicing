c5295b4238d77fbfd698b0d8c6b3fbb48e1c8782
badr <bernardoantoniod@gmail.com>
2025-04-11 16:18:23 -0400

    change the connection with the database. Now I'm using context manager and I have to delete the close connection

--- background/backendCem6/database_helpers.py
+++ background/backendCem6/database_helpers.py
@@ -53,113 +53,120 @@ connect_db.close()
 
 # Function to add a user to the database using the User instance and then to be used on post method:
 def add_user_to_db(new_user):
-    connect_db = sqlite3.connect(database_name)
-    cursor_db = connect_db.cursor()
-    cursor_db.execute(
-        """
-        INSERT INTO users (first_name, last_name, rut, phone, email, address, sensor_id)
-        VALUES (?, ?, ?, ?, ?, ?, ?)
-        """, (new_user.first_name, new_user.last_name, new_user.rut, new_user.phone, new_user.email, new_user.address, new_user.sensor_id)
-    )
-    connect_db.commit()
-    connect_db.close()
+    # connect_db = sqlite3.connect(database_name)
+    # Better way to connect to prevent errors while connecting: use the context manager:
+    with sqlite3.connect(database_name) as connect_db:
+        cursor_db = connect_db.cursor()
+        cursor_db.execute(
+            """
+            INSERT INTO users (first_name, last_name, rut, phone, email, address, sensor_id)
+            VALUES (?, ?, ?, ?, ?, ?, ?)
+            """, 
+            (
+                new_user.first_name,
+                new_user.last_name,
+                new_user.rut,
+                new_user.phone,
+                new_user.email,
+                new_user.address, 
+                new_user.sensor_id
+            )
+        )
+        connect_db.commit()
+        # Now we don't need to close the connection, because we are using the context manager:
+        #connect_db.close()
     return {"message": "User added successfully", "user": new_user.dict()}
+
 
 # Insert values from devices into the database:
 def insert_lectures(lectures, sensor_id, time_stamp):
-    connect_db = sqlite3.connect(database_name)
-    cursor_db = connect_db.cursor()
-    lectures.append(sensor_id)
-    lectures.append(time_stamp)
-    cursor_db.execute( """
-    INSERT INTO lectures ( voltage_V, current_A, frecuency_Hz, active_power_W, reactive_power_var, aparent_power_VA, power_factor, active_energy_consumption_kWh, sensor_id, lecture_time)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    """, (lectures))
-    connect_db.commit()
-    connect_db.close()
+    with sqlite3.connect(database_name) as connect_db:
+        cursor_db = connect_db.cursor()
+        lectures.append(sensor_id)
+        lectures.append(time_stamp)
+        cursor_db.execute(
+            """
+            INSERT INTO lectures ( voltage_V, current_A, frecuency_Hz, active_power_W, reactive_power_var, aparent_power_VA, power_factor, active_energy_consumption_kWh, sensor_id, lecture_time)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """,
+            (
+                lectures
+            )
+        )
+        connect_db.commit()
 
 # Read energy from a device in a selected range of time:
 def energy_by_id_and_range(sensor_id, start_time, end_time):
-    connect_db = sqlite3.connect(database_name)
-    cursor_db = connect_db.cursor()
-    cursor_db.execute("""
-    SELECT MAX(active_energy_consumption_kWh) - MIN(active_energy_consumption_kWh) FROM lectures WHERE sensor_id = ? AND lecture_time BETWEEN ? AND ?
-    """, (sensor_id, start_time, end_time))
-    energy_consumption = cursor_db.fetchone()
-    connect_db.close()
+    with sqlite3.connect(database_name) as connect_db:
+        cursor_db = connect_db.cursor()
+        cursor_db.execute(
+            """
+            SELECT MAX(active_energy_consumption_kWh) - MIN(active_energy_consumption_kWh) FROM lectures WHERE sensor_id = ? AND lecture_time BETWEEN ? AND ?
+            """,
+            (
+                sensor_id,
+                start_time,
+                end_time
+            )
+        )
+        energy_consumption = cursor_db.fetchone()
+        connect_db.commit()
     return energy_consumption[0]
 
 # Function to add the monthly energy consumption to the historical_lectures table every month:
 def add_monthly_consumption_to_db(sensor_id, month, year, monthly_energy_consumption):
-    connect_db = sqlite3.connect(database_name)
-    cursor_db = connect_db.cursor()
-    cursor_db.execute(
-        """
-        INSERT INTO historical_lectures (sensor_id, month, year, monthly_consumption)
-        VALUES (?, ?, ?, ?)
-        """, (sensor_id, month, year, monthly_energy_consumption)
-    )
-    connect_db.commit()
-    connect_db.close()
+    with sqlite3.connect(database_name) as connect_db:
+        cursor_db = connect_db.cursor()
+        cursor_db.execute(
+            """
+            INSERT INTO historical_lectures (sensor_id, month, year, monthly_consumption)
+            VALUES (?, ?, ?, ?)
+            """,
+            (
+                sensor_id,
+                month, year,
+                monthly_energy_consumption
+            )
+        )
+        connect_db.commit()
 
 # Funtion to query all the data we need to print the invoice:
 def bring_invoice_data(first_day_previous_month, last_day_previous_month, sensor_id):
-    connect_db = sqlite3.connect(database_name)
-    cursor_db = connect_db.cursor()
-    # Bring data from lectures table:
-    cursor_db.execute(
-        """
+    with sqlite3.connect(database_name) as connect_db:
+        cursor_db = connect_db.cursor()
+        # Bring data from lectures table:
+        cursor_db.execute(
+            """
             SELECT MIN(active_energy_consumption_kWh), MAX(active_energy_consumption_kWh)
             FROM lectures
             WHERE sensor_id = ? AND lecture_time BETWEEN ? AND ?
-        """,(int(sensor_id), str(first_day_previous_month), str(last_day_previous_month))
-    )
-    data_from_lectures = cursor_db.fetchone()
-    first_month_lecture = data_from_lectures[0]
-    last_month_lecture = data_from_lectures[1]
-    month_energy_consumption = (last_month_lecture - first_month_lecture) # In kWh
-    monthly_cost = month_energy_consumption * 123
-    # Now bring the info of the customer:
-    cursor_db.execute(
-        """
+            """,
+            (
+                int(sensor_id),
+                str(first_day_previous_month),
+                str(last_day_previous_month)
+            )
+        )
+        data_from_lectures = cursor_db.fetchone()
+        first_month_lecture = data_from_lectures[0]
+        last_month_lecture = data_from_lectures[1]
+        month_energy_consumption = (last_month_lecture - first_month_lecture) # In kWh
+        monthly_cost = month_energy_consumption * 123
+        # Now bring the info of the customer:
+        cursor_db.execute(
+            """
             SELECT  sensor_id, first_name, last_name, address
             FROM users
             WHERE sensor_id = ?
-        """, (sensor_id,))
-    data_from_users = cursor_db.fetchone()
-    client_num = data_from_users[0]
-    client_first_name = data_from_users[1]
-    client_last_name = data_from_users[2]
-    client_address = data_from_users[3]
-    connect_db.close()
+            """,
+            (
+                sensor_id,
+            )
+        )
+        data_from_users = cursor_db.fetchone()
+        client_num = data_from_users[0]
+        client_first_name = data_from_users[1]
+        client_last_name = data_from_users[2]
+        client_address = data_from_users[3]
+        connect_db.commit()
     return [first_month_lecture, last_month_lecture, month_energy_consumption, monthly_cost, client_num, client_first_name, client_last_name, client_address]
-
-# async def bring_invoice_data(start_time, end_time, sensor_id):
-#     try:
-#         conn = sqlite3.connect("energy_consumption.db")
-#         cursor_db = conn.cursor()
-        
-#         # Log the query parameters
-#         logging.info(f"Fetching invoice data for sensor_id={sensor_id}, start_time={start_time}, end_time={end_time}")
-        
-#         # Format the query
-#         cursor_db.execute(
-#             """
-#             SELECT MIN(active_energy_consumption_kWh), MAX(active_energy_consumption_kWh)
-#             FROM lectures
-#             WHERE sensor_id = ? AND lecture_time BETWEEN ? AND ?
-#             """,
-#             (sensor_id, start_time, end_time)
-#         )
-        
-#         invoice_data = cursor_db.fetchone()
-#         conn.close()
-        
-#         if not invoice_data:
-#             logging.error("No data found for the specified sensor_id and date range.")
-#             return None
-        
-#         return invoice_data
-#     except sqlite3.OperationalError as e:
-#         logging.error(f"Database error: {e}")
-#         raise
